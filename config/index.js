7d4ea2d3361c3890251a29a293203d57c704a494
Jasmin Alimanovic <jasmin.alimanovic2018@size.ba>
2021-05-04 02:24:34 +0200

    Fixed bugs

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://jsonplaceholder.typicode.com/todos"
-  : "https://jsonplaceholder.typicode.com/todos";
+  : "https://web-portal1.vercel.app/";
